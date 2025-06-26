4c4e500b1e6057b40d171aea89234c7fb2d2738c
2001wlf <1099851510@qq.com>
2025-06-26 20:21:39 +0800

    genearte picture of model trainset

--- pickle_read.py
+++ pickle_read.py
@@ -2,25 +2,25 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
-
-def draw_dataset(data):
+import argparse
+def draw_dataset(data,id):
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111)
     
     # Assuming data is a 2D array with shape (n_samples, n_features)
     x = data[:, 0]
     y = data[:, 1]
-    z = data[:, 2] if data.shape[1] > 2 else np.zeros_like(x)  # Use zeros if no z-dimension
+    #z = data[:, 2] if data.shape[1] > 2 else np.zeros_like(x)  # Use zeros if no z-dimension
 
-    ax.scatter(x, y, z)
+    ax.scatter(x, y)
     
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    
-    plt.show()
+    #ax.set_zlabel('Z Label')
+    #plt.show()
+    plt.savefig(f"radius/mydata2/picture/{args.dist}/" + str(data.shape[0]) + f"{id}.png")
 def read_data(n_node,k):
-    mydata = pickle.load(open("radius/mydata2/train/" + str(n_node) + ".pkl", "rb"))
+    mydata = pickle.load(open("radius/mydata2/val/" + str(n_node) + ".pkl", "rb"))
     node_feat = mydata["node_feat"]
     edge_feat = mydata["edge_feat"]
     edge_index = mydata["edge_index"]
@@ -28,11 +28,14 @@ def read_data(n_node,k):
     density_feat = mydata["density_feat"]
     for i in range(k):
         now_data=node_feat[i]
-        draw_dataset(now_data)
+        draw_dataset(now_data,id=i)
     return node_feat, edge_feat, edge_index, inverse_edge_index, density_feat
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Read and visualize dataset')
+    parser.add_argument('--dist', type=str, choices=['uniform', 'gm', 'subg', 'exp', 'student'], default='uniform')
+    args = parser.parse_args()
     n_node = 100
-    k = 3
+    k = 10
     node_feat, edge_feat, edge_index, inverse_edge_index, density_feat = read_data(n_node, k)
     print("Node Feature Shape:", node_feat.shape)
     print("Edge Feature Shape:", edge_feat.shape)
